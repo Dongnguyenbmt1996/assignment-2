4697b1504b9188c20153250831e0975e2a25fae3
Dongnguyenbmt1996 <131173013+Dongnguyenbmt1996@users.noreply.github.com>
2023-10-04 15:02:08 +0700

    storage.js

--- script/storage.js
+++ script/storage.js
@@ -8,17 +8,26 @@ navAnimation.addEventListener("click", function () {
 
 ///Lưu dữ liệu dưới LocalStorage
 
-//lấy dữ liệu petarr
-if (!getFromStorage("petArr")) {
-  //gắn dữ liệu
-  saveToStorage("petArr");
-}
-const petArr1 = getFromStorage("petArr");
+// //lấy dữ liệu petarr
+// if (!getFromStorage("petArr")) {
+//   //gắn dữ liệu
+//   saveToStorage("petArr");
+// }
+// const petArr1 = getFromStorage("petArr");
 
+// //Hàm lấy dữ liệu
+// function getFromStorage(key) {
+//   return JSON.parse(localStorage.getItem(key));
+// }
+// //Hàm lưu dữ liệu
+// function saveToStorage(key, value) {
+//   localStorage.setItem(key, JSON.stringify(value));
+// }
 //Hàm lấy dữ liệu
 function getFromStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
+
 //Hàm lưu dữ liệu
 function saveToStorage(key, value) {
   localStorage.setItem(key, JSON.stringify(value));
